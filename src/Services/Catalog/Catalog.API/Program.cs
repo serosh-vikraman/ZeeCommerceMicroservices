065e871187d0d50c0b48249c6fd83cfe654bc4ec
serosh-vikraman <serosh.vikraman@outlook.com>
2023-06-06 02:29:08 +0530

    Catalog API Creation

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,42 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+});
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddSingleton<IMongoClient>(a =>
+{
+    return new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+});
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
